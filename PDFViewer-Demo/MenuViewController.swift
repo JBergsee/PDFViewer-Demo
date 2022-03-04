000e69e2c73f895dfee92d18a0779dfed7f9efac
Johan Bergsee <johan@jnavionics.com>
2022-03-04 22:31:04 +0100

    Fixed a bug where a new outline button is added to the nav bar every time the view shows (if cached instead of recreated each time).

--- PDFViewer-Demo/MenuViewController.swift
+++ PDFViewer-Demo/MenuViewController.swift
@@ -12,6 +12,8 @@ import PDFViewer
 class MenuViewController: UIViewController {
     
    
+    var pdfView:PDFViewController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -22,9 +24,29 @@ class MenuViewController: UIViewController {
         if ((docURL) != nil) {
             pdfDocument = PDFDocument(url: docURL!)
         }
-        let vc = PDFViewController()
+        let vc = PDFViewController(showOutline: false)
         vc.pdfDocument = pdfDocument
         vc.pdfTitle = "Example PDF"
         self.navigationController?.pushViewController(vc, animated: true)
     }
+    
+    @IBAction func openSavedPDFView() {
+        if (pdfView == nil) {
+            //Initialize the view controller first time
+            let docURL = Bundle.main.url(forResource: "file-example_PDF_1MB", withExtension: "pdf")
+            var pdfDocument:PDFDocument?
+            if ((docURL) != nil) {
+                pdfDocument = PDFDocument(url: docURL!)
+            }
+            let vc = PDFViewController(showPrint: false)
+            vc.pdfDocument = pdfDocument
+            vc.pdfTitle = "Example PDF"
+            
+            //Save the viewController
+            pdfView = vc
+
+        }
+        self.navigationController?.pushViewController(pdfView!, animated: true)
+    }
+    
 }
